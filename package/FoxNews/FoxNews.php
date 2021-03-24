6fc3ef522f962b8a6b6018fb85fce51fff95d715
Ahmed Ashraf <ahmed29329@gmail.com>
2021-03-24 23:10:06 +0100

    check if data files does not exist

--- package/FoxNews/FoxNews.php
+++ package/FoxNews/FoxNews.php
@@ -2,13 +2,21 @@
 
 namespace Package\FoxNews;
 
+use RuntimeException;
+
 class FoxNews
 {
 	private $data = [];
 
 	public function __construct()
 	{
-		$this->data = json_decode(file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'foxnews.json'), true);
+		$path = __DIR__.DIRECTORY_SEPARATOR.'foxnews.json';
+		if(! file_exists($path)) {
+			throw new RuntimeException("File data is not found.");
+		}
+		
+		$content = file_get_contents($content);
+		$this->data = json_decode($content, true);
 	}
 
 	public function getNewsFromAPI()

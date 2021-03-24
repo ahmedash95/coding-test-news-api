bf9f7853404c9046a69af1b0ea6027be2a650479
Ahmed Ashraf <ahmed29329@gmail.com>
2021-03-24 23:28:15 +0100

    fix missing var

--- package/FoxNews/FoxNews.php
+++ package/FoxNews/FoxNews.php
@@ -14,9 +14,8 @@ class FoxNews
 		if(! file_exists($path)) {
 			throw new RuntimeException("File data is not found.");
 		}
-		
-		$content = file_get_contents($content);
-		$this->data = json_decode($content, true);
+
+		$this->data = json_decode(file_get_contents($path), true);
 	}
 
 	public function getNewsFromAPI()

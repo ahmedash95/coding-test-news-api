dbde330f8125c5e1003d680bf93fe4c2ef3459a2
ahmed soliman <ahmedsliman@gmail.com>
2021-09-12 23:47:54 +0200

    Refactoring into abstract

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
-use App\NewsAggregator;
+use App\FoxNewsAggregator;
+use App\NYTNewsAggregator;
+use Package\FoxNews\FoxNews;
+use Package\NYTimes\NewYorkTimes;
 
 require __DIR__.DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
 
-$aggregator = new NewsAggregator();
-$news = $aggregator->get();
+$aggregatorFox = new FoxNewsAggregator(new FoxNews());
+$aggregatorNYT = new NYTNewsAggregator(new NewYorkTimes());
 
-print_r($news);
+print_r(array_merge(
+    $aggregatorFox->get(),
+    $aggregatorNYT->get()
+));
---
+++ src/NYTNewsAggregator.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App;
+
+
+use Package\FoxNews\FoxNews;
+use Package\NYTimes\NewYorkTimes;
+
+class NYTNewsAggregator
+{
+	private $news;
+	private array $articles;
+
+	public function __construct(NewYorkTimes $source)
+	{
+		$this->news = $source->getNews()->articles;
+	}
+
+	public function get() :array
+	{
+		foreach ($this->news as $row) {
+			$this->articles[] = [
+				'title' => (string) $row->title,
+				'author' => (string) $row->author,
+				'image' => (string) $row->image,
+				'publish_date' => (string) $row->published_at,
+				'source' => (string) $row->source,
+				'url' => (string) $row->url,
+			];
+		}
+
+		return $this->articles;
+	}
+}
